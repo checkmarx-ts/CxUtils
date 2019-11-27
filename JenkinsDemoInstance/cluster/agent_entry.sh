90dd888943431a23e8de892fb71cec90378e985c
Nathan Leach <nathan.leach@checkmarx.com>
2019-11-27 07:46:48 -0600

    improving logging

--- JenkinsDemoInstance/cluster/agent_entry.sh
+++ JenkinsDemoInstance/cluster/agent_entry.sh
@@ -4,10 +4,10 @@ if [ $# -gt 0 ];
 then
     bash -c "$@"
 else
+    sleep 30s
     wget http://jenkins-master:8080/jnlpJars/agent.jar
-    java $JAVA_OPTS -jar agent.jar \
+    java $JAVA_OPTS -Djava.util.logging.config.file=/logging.properties -jar agent.jar \
         -jnlpUrl http://jenkins-master:8080/computer/$AGENT_TAG/slave-agent.jnlp \
         -workDir "/workspace" \
-        -loggingConfig /logging.properties \
         || rm -f agent.jar
 fi
