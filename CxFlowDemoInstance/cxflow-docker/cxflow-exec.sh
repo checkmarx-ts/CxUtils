5a591075a83b03ef5bdbd59d8532a86ad70cdd01
Nathan Leach <nathan.leach@checkmarx.com>
2020-03-23 06:35:00 -0500

    Added aids for cx-flow development deugging.

--- CxFlowDemoInstance/cxflow-docker/cxflow-exec.sh
+++ CxFlowDemoInstance/cxflow-docker/cxflow-exec.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export CXFLOW_JAR=`ls /*.jar`
+# Use the most recent jar based on version stamp
+export CXFLOW_JAR=`ls /*.jar | sort -r | head -n 1`
+
+echo Using CxFlow Jar: $CXFLOW_JAR
 
 java -Xms512m -Xmx2048m -Djava.security.egd=file:/dev/./urandom \
     -Dspring.profiles.active=web \
