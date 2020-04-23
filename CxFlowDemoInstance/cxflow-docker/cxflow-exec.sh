18467d92ec62f4c6d1365a37f1ab7a1249a19f84
Nathan Leach <nathan.leach@checkmarx.com>
2020-04-23 17:00:02 -0500

    Fix to work with latest cx-flow docker image changes

--- CxFlowDemoInstance/cxflow-docker/cxflow-exec.sh
+++ CxFlowDemoInstance/cxflow-docker/cxflow-exec.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # Use the most recent jar based on version stamp
-export CXFLOW_JAR=`ls /*.jar | sort -r | head -n 1`
+export CXFLOW_JAR=`ls /app/*.jar | sort -r | grep cx-flow- | head -n 1`
+if [ -z $CXFLOW_JAR ]; then
+    export CXFLOW_JAR=/app/cx-flow.jar
+fi
+
 
 echo Using CxFlow Jar: $CXFLOW_JAR
 
@@ -9,7 +13,7 @@ java -Xms512m -Xmx2048m -Djava.security.egd=file:/dev/./urandom \
     -Dspring.profiles.active=web \
     $JAVA_OPTS \
     -jar $CXFLOW_JAR --$CXFLOW_MODE \
-    --spring.config.location=/application-$CX_CONFIG.yml \
+    --spring.config.location=/app/application-$CX_CONFIG.yml \
     --app=cxflow-demo-docker \
     $@ 
 
