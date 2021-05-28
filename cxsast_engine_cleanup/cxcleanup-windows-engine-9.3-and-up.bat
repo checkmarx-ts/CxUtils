ddd8f39e59de4df8ba5f7a8fb36bce9b1d9571cc
Nir Weinberg <Nir.Weinberg@checkmarx.com>
2021-05-28 15:26:15 +1000

    updated engine scans path for 9.3 and up and removed quotes around the path to avoid the forfile ERROR: Invalid argument/option - '/F'.

--- cxsast_engine_cleanup/cxcleanup-windows-engine-9.3-and-up.bat
+++ cxsast_engine_cleanup/cxcleanup-windows-engine-9.3-and-up.bat
@@ -1,7 +1,6 @@
-REM For CxEngine Servers - modify paths according to Cx installation folders
+REM For EngineServiceScans - modify path according to CX_ES_ENGINE_SCANS_PARENT_PATH environment variable
 
 ECHO Removing engine scan files older than 7 days...
-forfiles -p "C:\Program Files\Checkmarx\Checkmarx Engine Server\Engine Server\Scans" -s -m *.* /D -7  /C "cmd /c del /F @path"
-forfiles -p "C:\Program Files\Checkmarx\Checkmarx Engine Server\Engine Server\Logs\ScanLogs" -s -m *.* /D -7  /C "cmd /c del /F @path"
+forfiles -p C:\EngineServiceScans\Scans\ -s -m *.* /D -7  /C "cmd /c del /f @PATH"
 
 ECHO Done!
