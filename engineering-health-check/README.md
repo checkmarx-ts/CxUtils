098b729282eabeaa228409c42a9dd881120e228c
James Bostock <77472194+james-bostock-cx@users.noreply.github.com>
2022-06-13 18:07:57 +0000

    Add scan result summary (#46)
    * Remove trailing white space
    
    * Rename getOData to getScanOData
    
    In preparation for adding a similar function for retrieving triage
    data.
    
    * Retrieve and summarize result data
    
    In addition to the scan data, the cxInsight_9_0.ps1 script now also
    retrieves and summarizes result data. This data is written to a
    separate file so the script now, as its output, generates a zip
    archive containing both the scan and the result data.
    
    The getScanOdata function has been changed to accept the output
    filename as its parameter so that this filename need only be specified
    in the one place (the new getResultOData function is the same).
    
    * Update EHC README.md
    
    Add description of the new functionality (retrieval of result data)
    and fix a typo.
    
    * Indicate operation in error message
    
    I.e., indicate whether the error occurred when retrieving scan or
    result data.
    
    * Indentation fix
    
    * Put results in a separate subordinate hash map
    
    This is a step towards generating the output format that Brent has
    requested.
    
    * Remove precomputed totals from results data
    
    * Format results as an array of projects instead of a map
    
    Requested by Brent, presumably because this format is easier to
    process in the Excel Workbook.
    
    * Update script introductory comment
    
    * Format result summary as a list of result states with counts
    
    Another request from Brent: instead of formatting the result summary
    as a map keyed on the result state name, we produce an array of result
    state objects, each with a StateName and Count property.
    
    * Add special results entry when there are no results
    
    Requested by Brent as this makes processing the data with Power Query
    easier.
    
    * Remove special entry for when there are no results
    
    Brent says he has fixed the spreadsheet so that it will handle an
    empty array.
    
    * Retrieve the project identifier instead of the project name
    
    Several clients have expressed unease at having their project names
    included in the export.
    
    * Add command line switch to control retrieval of result data
    
    Suggested by Nathan Leach: https://github.com/checkmarx-ts/CxUtils/pull/46#discussion_r886708085
    
    * Retrieve results for each project one at a time
    
    This is to prevent the OData query timing out when run against an
    instance with large amounts of data.
    
    * Write activity messages to the verbose message stream
    
    * Display progress bar when retrieving result data
    
    * Documentation updates and fixes
    
    Co-authored-by: Nathan Leach <nleach999@gmail.com>

--- engineering-health-check/README.md
+++ engineering-health-check/README.md
@@ -1,10 +1,11 @@
 # engineering-health-check
 
 These Powershell scripts are used to retrieve  CxSAST Scan Data for Insight Analysis in the Engineering Health Check.
-The script will collect scan Information that includes data about: Projects, Presets, Teams, Engines, and Result Metrics 
+The script will collect scan Information that includes data about: Projects, Presets, Teams, Engines, and Result Metrics
+The 9.x version of the script also optionally collects and generates a summary of the result data for the last scan of each project. Note that it is possible for the last scan of a project to have no results (e.g., if there were no code changes).
 
 * Use the corresponding script for the version of CxSAST that is installed.
-* Ensure admin priviledges before running the scripts
+* Ensure admin privileges before running the scripts
 
 
 cxInsight_XX.ps1 usage
@@ -18,7 +19,7 @@ Open powershell and enter the following replacing XX with the correct version
 
 ## Credential Prompt via Command Line
 
-The script prompts for a CxSAST username and password. If your environment does not have GUI support then you should first enable console prompting. 
+The script prompts for a CxSAST username and password. If your environment does not have GUI support then you should first enable console prompting.
 
 ```powershell
 $key = "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds"
--- engineering-health-check/cxInsight_9_0.ps1
+++ engineering-health-check/cxInsight_9_0.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 This is a Powershell script to retrieve your Checkmarx ScanData for Insight Analysis
 .DESCRIPTION
-This script will collect Scan Information that includes data about: Projects, Presets, Teams, Engines, and Result Metrics 
+This script will collect Scan Information that includes data about: Projects, Presets, Teams, Engines, and Result Metrics. It also optionally retrieves scan results and generates a summary of them.
 .PARAMETER cx_sast_server
     URL of the Checkmarx Server (i.e. https://companyname.checkmarx.net or http://localhost).
 .PARAMETER day_span
@@ -11,16 +11,24 @@ This script will collect Scan Information that includes data about: Projects, Pr
     The start date of the date range you would like to collect data (Format: yyyy-mm-DD)
 .PARAMETER end_date
     The end date of the date range you would like to collect data (Format: yyyy-mm-DD)
+.PARAMETER bypassProxy
+    If provided, the script will attempt to bypass any proxy when invoking the CxSAST API
+.PARAMETER results
+If provided, the script will retrieve and summarize result data as well as scan data
+.PARAMETER verbose
+If provided, the script will retrieve print activity messages
 .EXAMPLE
     C:\PS> .\cxInsight_9_0.ps1 -cx_sast_server https://customerurl.checkmarx.net
 .EXAMPLE
     C:\PS> .\cxInsight_9_0.ps1 -start_date 2019-04-01
 .EXAMPLE
     C:\PS> .\cxInsight_9_0.ps1 -cx_sast_server http://localhost -day_span 180
+.EXAMPLE
+C:\PS> .\cxInsight_9_0.ps1 -cx_sast_server http://localhost -results
 .NOTES
     Author: Checkmarx
     Date:   April 13, 2020
-    Updated: March 9, 2021
+    Updated: June 8, 2022
 #>
 
 param(
@@ -31,7 +39,10 @@ param(
     [String]$end_date = (Get-Date -format "yyyy-MM-dd"),
     [Parameter(Mandatory=$False)]
     [switch]
-    $bypassProxy
+    $bypassProxy,
+    [Parameter(Mandatory=$False)]
+    [switch]
+    $results
     )
 
 ###### Do Not Change The Following Configs ######
@@ -54,7 +65,8 @@ function getOAuth2Token() {
         client_id     = $clientId
         client_secret = $clientSecret
     }
-    
+
+    Write-Verbose "Retrieving OAuth2 token"
     try {
         if ($bypassProxy) {
             $response = Invoke-RestMethod -noProxy -uri "${serverRestEndpoint}auth/identity/connect/token" -method post -body $body -contenttype 'application/x-www-form-urlencoded'
@@ -70,47 +82,151 @@ function getOAuth2Token() {
 		Write-Host "Unable to retrieve Checkmarx AC Token"
         exit(-1)
     }
-    
+
     return $response.token_type + " " + $response.access_token
 }
 
-Write-Host "Running Script on Version " (get-host).Version 
+Write-Host "Running Script on Version " (get-host).Version
 $token = getOAuth2Token
 
-function getOdata() {
-    $Url = "${cx_sast_server}/cxwebinterface/odata/v1/Scans?`$select=Id,ProjectName,OwningTeamId,TeamName,ProductVersion,EngineServerId,Origin,PresetName,ScanRequestedOn,QueuedOn,EngineStartedOn,EngineFinishedOn,ScanCompletedOn,ScanDuration,FileCount,LOC,FailedLOC,TotalVulnerabilities,High,Medium,Low,Info,IsIncremental,IsLocked,IsPublic&`$expand=ScannedLanguages(`$select=LanguageName)&`$filter=ScanRequestedOn%20gt%20${start_date}Z%20and%20ScanRequestedOn%20lt%20${end_date}z"
+function getScanOdata {
+    param (
+        $outputFile
+    )
+    Write-Verbose "Retrieving scan data"
+
+    $Url = "${cx_sast_server}/cxwebinterface/odata/v1/Scans?`$select=Id,ProjectId,OwningTeamId,TeamName,ProductVersion,EngineServerId,Origin,PresetName,ScanRequestedOn,QueuedOn,EngineStartedOn,EngineFinishedOn,ScanCompletedOn,ScanDuration,FileCount,LOC,FailedLOC,TotalVulnerabilities,High,Medium,Low,Info,IsIncremental,IsLocked,IsPublic&`$expand=ScannedLanguages(`$select=LanguageName)&`$filter=ScanRequestedOn%20gt%20${start_date}Z%20and%20ScanRequestedOn%20lt%20${end_date}z"
     $headers = @{
         Authorization = $token
     }
     try {
         if ($bypassProxy) {
-            $response = Invoke-RestMethod -noProxy -uri "$Url" -method get -headers $headers -OutFile "data.txt"
+            $response = Invoke-RestMethod -noProxy -uri "$Url" -method get -headers $headers -OutFile $outputFile
         }
         else {
-            $response = Invoke-RestMethod -uri "$Url" -method get -headers $headers -OutFile "data.txt"
+            $response = Invoke-RestMethod -uri "$Url" -method get -headers $headers -OutFile $outputFile
         }
-        return $response
     }
     catch {
         Write-Host "Exception:" $_
         Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__
         Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
-		Write-Host $Url
-		Write-Host "An Error has prevented this script from collecting Odata."
+	Write-Host $Url
+	Write-Host "An error has prevented this script from collecting scan Odata."
+        exit(-1)
+    }
+}
+
+function getResultOData {
+    param (
+        $outputFile
+    )
+    Write-Verbose "Retrieving result data"
+
+    $Url = "${cx_sast_server}/cxwebinterface/odata/v1/Projects?`$select=Id"
+    $headers = @{
+        Authorization = $token
+    }
+    try {
+        if ($bypassProxy) {
+            $response = Invoke-RestMethod -noProxy -uri "$Url" -method get -headers $headers
+        }
+        else {
+            $response = Invoke-RestMethod -uri "$Url" -method get -headers $headers
+        }
+
+        $states = @{
+            "0" = "To Verify"
+            "1" = "Not Explotable"
+            "2" = "Confirmed"
+            "3" = "Urgent"
+            "4" = "Proposed Not Exploitable"
+        }
+        $projects = @{}
+        $response | Select-Object -ExpandProperty Value | ForEach-Object {
+            $projectId = "$($_.Id)"
+            Write-Progress -Activity "Retrieving Results"
+            Write-Verbose "Retrieving result data for project ${projectId}"
+
+            $Url = "${cx_sast_server}/cxwebinterface/odata/v1/Projects(${projectId})?`$select=Id&`$expand=LastScan(`$select=Id;`$expand=Results(`$select=Id,ResultId,StateId))"
+            if ($bypassProxy) {
+                $response = Invoke-RestMethod -noProxy -uri "$Url" -method get -headers $headers
+            }
+            else {
+                $response = Invoke-RestMethod -uri "$Url" -method get -headers $headers
+            }
+
+            $response | Select-Object -ExpandProperty Value | ForEach-Object {
+                $projectId = "$($_.Id)"
+                if ( -not $projects.ContainsKey($projectId) ) {
+                    $projects[$projectId] = @{
+                        LastScanId = $_.LastScan.Id
+                        Results = @{}
+                    }
+                }
+
+                Foreach ( $result in $_.LastScan.Results ) {
+                    $stateId = "$($result.StateId)"
+                    if ( -not $states.ContainsKey($stateId) ) {
+                        $states[$stateId] = "Custom State $($result.StateId)"
+                    }
+                    $stateName = $states[$stateId]
+                    $projects[$projectId]['Results'][$stateName] = $projects[$projectId]['Results'][$stateName] + 1
+                }
+            }
+        }
+
+        # Convert projects hashmap to an array
+        $newProjects = @()
+        Foreach ( $projectId in $projects.Keys ) {
+            $project = @{
+                ProjectId = $projectId
+                LastScanId = $projects.$projectId.LastScanId
+                Results = @()
+            }
+            Foreach ( $stateName in $projects.$projectId.Results.Keys ) {
+                $result = @{
+                    StateName = $stateName
+                    Count = $projects.$projectId.Results.$stateName
+                }
+                $project.Results += $result
+            }
+            $newProjects += $project
+        }
+
+        $response = @{
+            "Projects" = $newProjects
+        }
+
+        $response | ConvertTo-Json -Compress -Depth 4 | Out-File -FilePath $outputFile -Encoding utf8
+    }
+    catch {
+        Write-Host "Exception:" $_
+        Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__
+        Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+	Write-Host $Url
+	Write-Host "An error has prevented this script from collecting result Odata."
         exit(-1)
     }
 }
 
 try
 {
-    $data = getOdata('')
-    Read-Host -Prompt "The script was successful. Please send the 'data.txt' file in this directory to your Checkmarx Engineer. Press Enter to exit"
+    $files = @(".\scan-data.json")
+    getScanOdata($files[0])
+    if ( $results) {
+        $files += ".\result-data.json"
+        getResultOdata($files[1])
+    }
+    Compress-Archive -Path $files -DestinationPath ".\data.zip" -Force
+    Remove-Item -Path $files
+    Read-Host -Prompt "The script was successful. Please send the 'data.zip' file in this directory to your Checkmarx Engineer. Press Enter to exit"
 }
 catch
 {
     Write-Host "Exception:" $_
     Write-Error $_.Exception.ToString()
-    Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+    Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__
     Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
     Read-Host -Prompt "The above error occurred. Press Enter to exit."
 }
